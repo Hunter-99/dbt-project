e75ea8199370ecb26468b696b380f7c64741870f
Andrew Chuiko <andrewpro950@gmail.com>
2023-02-24 10:53:05 +0000

    Added fact_fhv_trips

--- models/staging/stg_fhv_tripdata.sql
+++ models/staging/stg_fhv_tripdata.sql
@@ -1,5 +1,11 @@
 {{ config(materialized='view') }}
 
 select 
-    *
-from {{source('staging', 'fhv_tripdata')}}
+    dispatching_base_num as dispatching_base_num,
+    pickup_datetime	as pickup_datetime,
+    dropOff_datetime as dropoff_datetime,	
+    PUlocationID as pu_location_id,
+    DOlocationID as do_location_id,		
+    SR_Flag	as sr_flag,
+    Affiliated_base_number as affiliated_base_number
+from {{ source('staging', 'fhv_tripdata') }}
